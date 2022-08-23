c3aca6dd12b24da6e57ee995aee4a2923f0b510d
LexieLiu19 <xuliu0925@gmail.com>
2022-08-23 16:56:41 -0400

    initial commit 🚀🚀🚀🚀

---
+++ src/com/LeetCodeSolutions/ArraysAndHashing/ContainsDuplicate/Solution.java
@@ -0,0 +1,7 @@
+package com.LeetCodeSolutions.ArraysAndHashing.ContainsDuplicate;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/com/LeetCodeSolutions/Main.java
@@ -0,0 +1,8 @@
+package com.LeetCodeSolutions;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}

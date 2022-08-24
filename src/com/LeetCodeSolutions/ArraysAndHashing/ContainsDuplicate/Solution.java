0d343755966582f3669c6168efddc4b06e0579ac
LexieLiu19 <xuliu0925@gmail.com>
2022-08-24 10:04:22 -0400

    Add 217 ContainsDuplicate

--- src/com/LeetCodeSolutions/ArraysAndHashing/ContainsDuplicate/Solution.java
+++ src/com/LeetCodeSolutions/ArraysAndHashing/ContainsDuplicate/Solution.java
@@ -1,7 +1,27 @@
 package com.LeetCodeSolutions.ArraysAndHashing.ContainsDuplicate;
 
-public class Solution {
-    public static void main(String[] args) {
 
+import java.util.HashSet;
+import java.util.Set;
+
+public class Solution {
+
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[] nums = {1, 2, 3, 4, 3};
+        System.out.println(s.containsDuplicate(nums));
+    }
+
+    public boolean containsDuplicate(int[] nums) {
+
+        Set<Integer> unique = new HashSet<>();
+
+        for (var num : nums) {
+            if (unique.contains(num)) return true;
+            unique.add(num);
+        }
+
+        return false;
     }
 }
